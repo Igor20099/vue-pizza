f8f9f5a20dc748f74249ab5b0254183e58ef3ee4
Igor <megakill123@yandex.ru>
2024-05-02 20:51:11 +0500

    start project

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="wrapper">
+    <HeaderComponent />
+  </div>
+</template>
+
+<script setup>
+import HeaderComponent from "./components/HeaderComponent.vue";
+</script>
+
+<style scoped lang="scss">
+.wrapper {
+  background-color: #fff;
+  width: calc(100vw - 100px);
+  margin: 0 auto;
+  border-radius: 10px;
+}
+</style>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,75 @@
+<template>
+  <header class="header">
+    <div class="wrapper">
+      <img class="logo" src="../assets/img/logo.png" />
+
+      <div class="content">
+        <h1 class="title">VUE PIZZA</h1>
+        <p class="description">самая вкусная пицца во вселенной</p>
+      </div>
+    </div>
+
+    <button class="btn-cart">
+      <span> 500 руб. |</span>
+      <img class="btn-image" src="../assets/img/cart.svg" /> <span>0</span>
+    </button>
+  </header>
+</template>
+
+<script setup></script>
+
+<style scoped lang="scss">
+.header {
+  padding: 20px;
+  display: flex;
+  justify-items: center;
+  align-items: center;
+
+  .wrapper {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    .logo {
+      width: 38px;
+      height: 38px;
+      margin-right: 8px;
+    }
+
+    .content {
+      display: flex;
+      flex-direction: column;
+
+      .title {
+        font-size: 24px;
+        margin-left: 16px;
+      }
+    }
+  }
+
+  .btn-cart {
+    margin-left: auto;
+    background-color: #fe5f1e;
+    color: #fff;
+    padding: 16px 23px;
+    border: none;
+    cursor: pointer;
+    border-radius: 16px;
+    font-size: 16px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 8px;
+    transition: all 0.4s ease-in-out;
+
+    .btn-img {
+      width: 16px;
+      height: 16px;
+    }
+  }
+
+  .btn-cart:hover {
+    background-color: #c94d1c;
+  }
+}
+</style>
